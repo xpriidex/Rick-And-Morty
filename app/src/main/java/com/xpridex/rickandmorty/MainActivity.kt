5665deebe6268929e9873e63f4534b0c256551cd
xpridex <lopezarcefelipe@gmail.com>
2021-05-19 18:37:11 -0400

    setup navigation between fragments

--- app/src/main/java/com/xpridex/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/xpridex/rickandmorty/MainActivity.kt
@@ -2,10 +2,46 @@ package com.xpridex.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.xpridex.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var navHostFragment = NavHostFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupActivity()
+    }
+
+    private fun setupActivity() {
+        setUpNavigation()
+        destinationManager()
+    }
+
+    private fun setUpNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navGraph = navHostFragment.navController.graph
+        navHostFragment.navController.setGraph(navGraph, intent.extras)
+        appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+    }
+
+    private fun destinationManager() {
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.slashFragment -> supportActionBar?.hide()
+                else -> {
+                    supportActionBar?.show()
+                }
+            }
+        }
     }
 }
